335484b76d0459de10dff43bfedff5e506f27373
MaelVB <vaillant.beuchot.mael@gmail.com>
2022-05-03 18:23:52 +0200

    fix: Fetch stats

--- main.js
+++ main.js
@@ -32,24 +32,27 @@ const updateStats = async () => {
 }
 
 const fetchStats = async () => {
-    const brutStatusStats = await got('https://mcapi.us/server/status?ip=' + config.default.minecraftServerUrl).json();
-    const brutQueryStats = await got('https://mcapi.us/server/query?ip=' + config.default.minecraftServerUrl).json();
-    brutQueryStats.server = brutStatusStats.server;
-    return brutQueryStats;
+    const brutStats = await got('https://api.mcsrvstat.us/2/' + config.default.minecraftServerUrl).json();
+    return brutStats;
+}
+
+const currentPlayersStats = async (brutStats) => {
+    const stats = brutStats.players.online + " sur " + brutStats.players.max
+    return stats;
 }
 
 const stats = async (brutStats) => {
     if(brutStats.online) {
         let players = "\n";
-        if (brutStats.players.now > 0) {
-            players = players + await playersStats();
+        if (brutStats.players.online > 0) {
+            players = players + " " + await playersStats();
         }
         const d = new Date();
         const hour = d.getHours();
         const min = d.getMinutes();
         const time = hour + ":" + (min < 10 ? '0' : '') + min
-        let stats = '[' + time + ']\nServeur **EN LIGNE**' + "\nVersion : " + brutStats.server.name + "\nJoueurs : " + brutStats.players.now + "/" + brutStats.players.max
-        let pluginsNames = brutStats.plugins.join('\n');
+        let stats = '[' + time + ']\nServeur **EN LIGNE**' + "\nVersion : " + brutStats.version + " (" + brutStats.software + ")" + "\nJoueurs : " + brutStats.players.online + "/" + brutStats.players.max
+        let pluginsNames = brutStats.plugins.raw.join('\n');
         stats = stats + players + '\n\nPlugins : \n`' + pluginsNames + '`'
         return stats;
     } else {
@@ -73,11 +76,6 @@ const playersStats = async () => {
     return players;
 }
 
-const currentPlayersStats = async (brutStats) => {
-    const stats = brutStats.players.now + " sur " + brutStats.players.max
-    return stats;
-}
-
 const dynmapStats = async () => {
     let brutStats = await got(config.default.dynmapUrl).json();
     return brutStats;
@@ -86,10 +84,10 @@ const dynmapStats = async () => {
 // EVENTS
 
 client.on("ready", async function () {
-    console.log("Ratuscraft Bot launched !");
-    /*if(config.default.cronIsActive) {
+    console.log("Alsy Minecraft Bot launched !");
+    if(config.default.cronIsActive) {
         changePlayersChan.start();
-    }*/
+    }
 })
 
 client.on("message", async (message) => {
@@ -104,42 +102,9 @@ client.on("message", async (message) => {
         let brutStats = ""
 
         // Role admin requis
-        if (message.member.roles.cache.some(r => r.name === "Organisateur") || message.member.roles.cache.some(r => r.name === "Le role des modos du bot")) {
+        if (message.member.roles.cache.some(r => r.name === "Modérateurs") || message.member.roles.cache.some(r => r.name === "Le role des modos du bot")) {
             switch (command) {
-                // Utile
-                case 'init':
-                    try {
-                        if(config.default.init) {
-                            const configFile = editJsonFile(`data/${configPath}`);
-
-                            if(config.default.createRulesChan) {
-                                const createRulesChan = await message.guild.channels.create('règles');
-                                configFile.set("discordRulesChanId", createRulesChan.id);
-
-                                const rulesMessageFile = require('./rulesMessage.json');
-                                const textRulesMessage1 = await message.guild.channels.cache.find(ch => ch.id === createRulesChan.id).send(rulesMessageFile.rulesMessage1);
-                                const textRulesMessage2 = await message.guild.channels.cache.find(ch => ch.id === createRulesChan.id).send(rulesMessageFile.rulesMessage2);
-                                const createRulesMessage = await message.guild.channels.cache.find(ch => ch.id === createRulesChan.id).send(rulesMessageFile.rulesMessage);
-                                configFile.set("discordRulesMessageId", createRulesMessage.id);
-                            }
-
-                            const createChan = await message.guild.channels.create('PENDING_INFORMATIONS');
-                            configFile.set("discordStatsChanId", createChan.id);
-
-                            const createMessage = await message.guild.channels.cache.find(ch => ch.id === createChan.id).send('PENDING_INFORMATIONS');
-                            configFile.set("discordStatsMessageId", createMessage.id);
-
-                            configFile.set("init", false);
-                            configFile.save();
-                            console.log("RESTART YOUR BOT")
-                        } else {
-                            message.channel.send(`L'initialisation a déjà été effectuée.`);
-                        }
-                    } catch(err) {
-                    console.error(err)
-                    }
-                    break;
-                
+                // Utile                
                 case 'update':
                     await updateStats();
                     break;
@@ -161,15 +126,6 @@ client.on("message", async (message) => {
                 }
         } else {
             console.log("Role non accepté");
-        }
-
-        // Role admin non requis
-        switch (command) {
-            // Amusant
-            case 'dice':
-                const diceRes = Math.floor(Math.random() * 7)
-                message.channel.send(`${diceRes}`);
-                break;
         }
     } catch (err) {
         console.error(err);
